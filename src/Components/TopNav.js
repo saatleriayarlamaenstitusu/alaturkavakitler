316a1a037a1d724a5fae210a9ad230606e6909d4
Abdullah <abdullahoguk@gmail.com>
2022-04-19 16:01:45 +0300

    inlinesvg added

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SVG from 'react-inlinesvg';
+
 
 const TopNav =()=>{
         return(
-            <div className=''>
-                TopNav
-            </div>
+            <div className='topNav'> 
+<SVG id="logoGenis" src={`${process.env.PUBLIC_URL}/Icons/logo-wide.svg`} ></SVG>           
+
+ </div>
         )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  node:'jit',
+  important: true,
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        "ust-pattern": "url('../../public/Icons/topbgtext.svg')",
+      
+      },
+
+
+    },
   },
   plugins: [],
 }
