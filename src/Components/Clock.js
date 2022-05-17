2689eef68d2f37c224bffb0a3316a15556f4e26e
Abdullah <abdullahoguk@gmail.com>
2022-05-17 08:21:09 +0300

    alaturka component

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,12 +1,13 @@
 import React, {useContext,useEffect, useState} from 'react'
 import { AppContext } from './Context'
+import { DateTime } from "luxon";
 
 
 function Clock(props){
     var {vakitler} = useContext(AppContext)
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(DateTime.now());
     useEffect(() => {
-        const clockInterval = setInterval(()=>setDate(new Date()), 1000);
+        const clockInterval = setInterval(()=>setDate(DateTime.now()), 1000);
         return function cleanup() {
             clearInterval(clockInterval);
           };
@@ -15,25 +16,33 @@ function Clock(props){
     /* TODO: Ayarlardan gelen varsayılan ve gösterilecek saate göre düzenlenecek */
     return(
         <div className="clock">
-            <div className="normalClock">{date.toLocaleTimeString("tr-tr")}</div>
-            <div className="alaturkaClock">
-
+            <div className="normalClock primaryClock">{date.setLocale('tr').toLocaleString(DateTime.TIME_SIMPLE)}</div>
+            <div className="alaturkaClock secondaryClock">
+            {calculateAlaturka(date,vakitler)}
             </div>
-            Akşam: { vakitler[getDateKey(date)] && vakitler[ getDateKey(date)].aksam}
+            Akşam: { vakitler[date.toISODate()] && vakitler[ date.toISODate()].aksam}
         </div>
     )
 }
 
 
-function getDateKey(date){return date.toISOString().split("T")[0]}
 
 //TODO 
-function calculateAlaturka(){
-    //normal saat akşam vakti ile gece yarısı arasınnda ise o günün akşam vakti
-    //    ns-av
-    //normal saat akşam vaktinden önceyse önceki günün akşam vakti
-    //    bgy-av+ns                  
+function calculateAlaturka(date, vakitler){
+var aksam=DateTime.fromSQL( date.toISODate() +" " + vakitler[ date.toISODate()].aksam)
 
+//normal saat akşam vaktinden önceyse önceki günün akşam vakti
+    //    bgy-av+ns 
+if(date < aksam)  {
+    var dunaksam = DateTime.fromSQL( date.minus({days:1}).toISODate() +" " + vakitler[ date.minus({days:1}).toISODate()].aksam) 
+   return date.diff(dunaksam).toFormat("hh:mm")
+}
+//normal saat akşam vakti ile gece yarısı arasınnda ise o günün akşam vakti
+    //    ns-av
+    
+else{
+    return date.diff(aksam).toFormat("hh:mm")
+}             
 
 }
 
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import Clock from "../Components/Clock"
 const HomePage =()=>{
         return(
-            <div className=''>
-            <Clock/>
+            <div className='px-5'>
+            <div className="mt-10"><Clock ></Clock></div>
                 HomePage
             </div>
         )
