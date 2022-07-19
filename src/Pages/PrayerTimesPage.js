8e4c833b18e5acda13e8f419a156d609a677d3d9
Abdullah <abdullahoguk@gmail.com>
2022-07-19 06:28:25 +0300

    prayer times page

--- src/Pages/PrayerTimesPage.js
+++ src/Pages/PrayerTimesPage.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Seo from "../Components/Seo"
+import VakitCounter from "../Components/VakitCounter"
+import VakitList from "../Components/VakitList"
 
 const PrayerTimesPage =(props)=>{
         return(
-            <div className=''>
+            <div className='PrayerTimesPage'>
                         <Seo title="Namaz Vakitleri" />
-                           <h1 className='topTitle'>{props.topTitle}. </h1> 
-                PrayerTimesPage
+                <h1 className='topTitle big mb-0 mt-5'>{props.topTitle}. </h1> 
+               <div className=" px-5"><VakitCounter/></div>
+            <div className="mt-3 mx-5 mb-5 "><VakitList/></div>
             </div>
         )
 }
